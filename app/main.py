304e23e77485d7cc226db9680aeda7e746bbb89a
Vivek Baidya <vivekbaidya22@gmail.com>
2026-04-08 00:43:05 +0530

    fix cors origin

--- app/main.py
+++ app/main.py
@@ -42,7 +42,7 @@ last_flights=[]
 last_search={}
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://sky-agent-gshb-mv1yb3enu-vivek9i080s-projects.vercel.app/"],
+    allow_origins=["https://sky-agent-gshb-mv1yb3enu-vivek9i080s-projects.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
