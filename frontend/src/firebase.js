5c4015dc5ed3256a731c9b930757cbc1d6b81a26
Vivek Baidya <vivekbaidya22@gmail.com>
2026-04-07 21:29:13 +0530

    firebase.js is changed

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { 
@@ -18,13 +19,13 @@ import {
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-  apiKey: "REACT_APP_FIREBASE_API_KEY",
-  authDomain: "REACT_APP_FIREBASE_AUTH_DOMAIN",
-  projectId: "REACT_APP_FIREBASE_PROJECT_ID",
-  storageBucket: "REACT_APP_FIREBASE_STORAGE_BUCKET",
-  messagingSenderId: "REACT_APP_FIREBASE_MESSAGING_SENDER_ID",
-  appId: "REACT_APP_FIREBASE_APP_ID",
-  measurementId: "REACT_APP_FIREBASE_MEASUREMENTID"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENTID
 };
 
 // Initialize Firebase
